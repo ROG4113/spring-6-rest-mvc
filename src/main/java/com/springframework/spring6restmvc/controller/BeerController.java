806ac7136ea6e13b683d57222e8ddfb882d6b5dc
ROG4113 <ass0009916@gmail.com>
2025-11-20 15:29:07 +0530

    lombok logging and testing

--- src/main/java/com/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/springframework/spring6restmvc/controller/BeerController.java
@@ -1,13 +1,25 @@
 package com.springframework.spring6restmvc.controller;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Controller;
 
+import com.springframework.spring6restmvc.model.Beer;
 import com.springframework.spring6restmvc.services.BeerService;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
     private final BeerService beerService;
+
+    public Beer getBeerById(UUID id){
+        
+        log.debug("Get Beer By Id - in controller");
+
+        return beerService.getBeerById(id);
+    }
 }
