852a7a2c64b798e0760bac55ca391ea552f42f4f
ROG4113 <ass0009916@gmail.com>
2025-11-21 13:22:50 +0530

    beer data

--- src/main/java/com/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/com/springframework/spring6restmvc/services/BeerService.java
@@ -1,9 +1,12 @@
 package com.springframework.spring6restmvc.services;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.springframework.spring6restmvc.model.Beer;
 
 public interface BeerService {
     Beer getBeerById(UUID id);
+
+    List<Beer> listBeer();
 }
