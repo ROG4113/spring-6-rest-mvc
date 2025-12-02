b55463d5b2f8de0e720f2101e0dd06dbb1dd0852
ROG4113 <ass0009916@gmail.com>
2025-12-02 21:21:32 +0530

    test update beer using mockmvc

--- src/test/java/com/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -2,8 +2,10 @@ package com.springframework.spring6restmvc.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -46,6 +48,19 @@ public class BeerControllerTest {
     @BeforeEach
     void setUp(){
         beerServiceImpl=new BeerServiceImpl();
+    }
+
+    @Test
+    void testUpdateBeer() throws Exception{
+        Beer beer=beerServiceImpl.listBeer().get(0);
+
+        mockMvc.perform(put("/api/v1/beer/" + beer.getId())
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(beer)))
+                .andExpect(status().isNoContent());
+
+        verify(beerService).updateBeerById(any(UUID.class), any(Beer.class));
     }
 
     @Test
