19590d6885fb30d6b94ede94a81411f11ffc3aa9
ROG4113 <as0009916@gmail.com>
2026-03-05 03:11:00 +0530

    adding relational entities

---
+++ src/main/java/com/springframework/spring6restmvc/entites/BeerOrderLine.java
@@ -0,0 +1,55 @@
+package com.springframework.spring6restmvc.entites;
+
+import java.security.Timestamp;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class BeerOrderLine {
+
+    @Id
+    @GeneratedValue(generator="UUID")
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+    public boolean isNew(){
+        return this.id==null;
+    }
+
+    private Integer orderQuantity=0;
+
+    private Integer quantityOnHand=0;
+}
